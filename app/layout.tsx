80a5d87804c22c6699edcbfbde35ebe922151093
Yogesh Pal <yogeshpal5049@gmail.com>
2026-06-18 22:43:53 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import { FaWhatsapp } from "react-icons/fa";
 
 const inter = Inter({
   variable: "--font-geist-sans",
@@ -28,7 +30,21 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${playfair.variable} antialiased`}
     >
-      <body className="min-h-screen">{children}</body>
+      <body className="min-h-screen">{children}
+         <div className="fixed z-40 bottom-4 right-2 md:right-6 flex flex-col gap-4">
+          <div className="relative w-14 h-14">
+            {/* Ripple Background */}
+            <span className="absolute inset-0 rounded-full bg-[#25D366] animate-ripple"></span>
+
+            {/* WhatsApp Button */}
+            <span className="absolute inset-0 flex items-center justify-center rounded-full bg-[#25D366] hover:scale-110 duration-300">
+              <Link href="https://wa.me/917042209741">
+                <FaWhatsapp className="text-2xl text-white" />
+              </Link>
+            </span>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
